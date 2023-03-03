037d1a2a01e0007d7d67787df75bbc4be26e0a0b
Teklu Mezgebo <mezge002@umn.edu>
2023-03-02 23:54:03 -0500

    commit tree d9e27214bf81fd16e3e7e68c6483b1eaa0ae0d22

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+  const winningRecords = record.find(record => record.result === "W")
+  return winningRecords ? winningRecords.year : undefined
+}
+
+
